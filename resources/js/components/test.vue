d2c3f24f42e00c43d2e4f407621da38cd4c33bd3
mantas <mantasruigys101@gmail.com>
2020-11-12 16:33:21 +0000

    vue working

---
+++ resources/js/components/test.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <div>
+            test
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
