05840dca820ba76b890e20bcc6b6464bc77ed710
Jakub <jakub.bebenek@gmail.com>
2020-11-12 17:45:58 +0000

    Installing d3

---
+++ resources/js/components/graph.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+
+        </div>
+    </div>
+</template>
+
+<script>
+    import * as d3 from 'd3';
+
+    export default {
+        name: "PackChart",
+        props: {
+            data: Object
+        },
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
