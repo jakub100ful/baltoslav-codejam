7df846a5c70454ab9ee9b4624471b5ce9b2d894c
Jakub <jakub.bebenek@gmail.com>
2020-11-12 17:23:45 +0000

    Dealing with merging

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,12 +8,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-<<<<<<< HEAD
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-=======
-        <link href="{{asset('js/sass/ap.scss')}}" rel="stylesheet">
-
->>>>>>> 5af6fe78012e791a3bd576698416bf3318181114
         <script src="{{ asset('js/app.js') }}" defer></script>
 
         
