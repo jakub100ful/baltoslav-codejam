6a639e34eecd90ee8f216cfa49f7f7da3c1b19c1
Jakub <jakub.bebenek@gmail.com>
2020-11-13 01:02:46 +0000

    Layout

--- resources/js/components/app.vue
+++ resources/js/components/app.vue
@@ -1,56 +1,122 @@
 <template>
     <div class="container-fluid">
-        <div>
-            <navbar/>
+        <div class="row row-lg-4 align-items-center h-100">
+            <div class="col col-lg">
+                <h1 class="text-center display-1">Dear Tech Companies...</h1>
+                <h1 class="text-center">We need to have a discussion about mental health.</h1>
+            </div>
         </div>
-        <div class="row p-3">
-            <card class="col-sm" headerText="Mental Health in Tech" bodyContent="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent aliquet ultrices malesuada. Aliquam aliquam, lacus at elementum tristique, dui erat posuere leo, at molestie ante ex et ante. Ut fermentum, odio ut feugiat consequat, felis ligula cursus neque, ac porta mi risus nec sem. Pellentesque cursus tempor justo. Proin porta interdum metus vitae molestie. Aenean euismod molestie est in faucibus. Aliquam id neque consequat, elementum orci vitae, volutpat sapien. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque at tincidunt dolor, ac auctor massa. Integer quis lectus tempor, feugiat neque non, cursus nulla. Aliquam tincidunt faucibus lorem, eget sagittis turpis luctus non. Proin in fermentum mi. Nulla facilisi. Vestibulum vitae arcu augue. Proin magna tellus, commodo eget vulputate commodo, tempor eu orci."/>
-        </div>
-        <div class="row">
-            <div class="col-sm">
+        <div class="row row-lg-4 justify-content-md-center">
+            <div class="col col-lg-4">
                 <div class="card">
-                    <test/>
+                    <div class="card-header">
+                        <h5>67% of people identified themselves to have had a mental health illness at some point</h5>
+                    </div>
+                    <div class="card-body">
+
+                    </div>
                 </div>
-                
+            </div>
+        </div>
+        <div class="row row-lg-4 d-flex flex-row justify-content-md-center">
+            <div class="col col-lg-4">
+                <div class="card">
+                    <div class="card-header">
+                        <h5>Company Populations</h5>
+                    </div>
+                    <div class="card-body">
+                        <test />
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row row-lg-4 d-flex flex-row justify-content-md-center">
+            <div class="col col-lg-6">
+                <div class="row-center">
+                    <div class="card">
+                        <div class="card-header align-self-center">
+                            <h3>67% of people identified themselves to have had a mental health illness at some point</h3>
+                        </div>
+                        <div class="card-body">
+
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
+<style scoped>
+.row-m-t{
+  margin-top : 50px
+}
+</style>
+
 <script>
     import * as d3 from 'd3';
     import card from './card.vue';
     import Navbar from './navbar.vue';
-import Graph from './graph.vue';
+    import Graph from './graph.vue';
+    import { ref, onMounted, onUnmounted } from 'vue';
 
     export default {
-  components: { card, Navbar, Graph },
-        data() {
-            return{
-                data: {},
+        setup() {
+            const foreground = ref(null);
+            const background = ref(null)
+
+            return {
+                foreground,
+                background
+            }
+
+            onMounted(() => {
+                document.addEventListener('scroll', handleScroll)
+            })
+
+            onUnmounted(() => {
+                document.removeEventListener('scroll', handleScroll)
+            })
+
+            const handleScroll = (evt) => {
+
+                const scrollY = window.scrollY
+                const maxBackgroundSize = 120;
+                const backgroundSize = ((scrollY) / (maxBackgroundSize - 100)) // increases as user scrolls
+
+                // zoom the background at a slower rate
+                background.value.style.transform = 'scale(' + (100 + backgroundSize * .4) / 100 + ')'
+                foreground.value.style.transform = 'scale(' + (100 + backgroundSize) / 100 + ')' 
             }
         },
-
-        mounted() {
-            console.log('Component mounted.');
-            this.getData();
-        },
-
-        methods:{
-            getData(){
-                    axios.get('/data/pop').then(rsp=>{
-                    this.data = rsp.data;
-                    
-                })
+        
+        components: { card, Navbar, Graph },
+            data() {
+                return{
+                    data: {},
+                }
             },
 
-            
-        },
-        computed: {
-            myStyles () {
-                return {height: '99px'}
+            mounted() {
+                console.log('Component mounted.');
+                this.getData();
+            },
+
+            methods:{
+                getData(){
+                        axios.get('/data/pop').then(rsp=>{
+                        this.data = rsp.data;
+                        
+                    })
+                },
+
+                
+            },
+            computed: {
+                myStyles () {
+                    return {height: '99px'}
+                }
             }
+            
         }
-        
-    }
 </script>
