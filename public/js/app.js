e83de77ae471a49510eb041a4d35d9cc2ac3a500
Jakub <jakub.bebenek@gmail.com>
2020-11-13 13:30:02 +0000

    Changes

--- public/js/app.js
+++ public/js/app.js
@@ -121988,8 +121988,8 @@ module.exports = "/images/Clouds.png?163ea25d9a85bcc001586589840d3bd8";
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\public_html\baltoslav-codejam\baltoslav\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\public_html\baltoslav-codejam\baltoslav\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\codejam\baltoslav-codejam\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\codejam\baltoslav-codejam\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
